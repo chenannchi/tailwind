956acc9ddd0d2fe8934132e939b6a34ced59ba28
chenannchi <anchichenusa@gmail.com>
2023-05-18 20:16:18 -0400

    add screen2

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './screens/Home';
+import Screen2 from './screens/Screen2';
 
-import {
-  SafeAreaView,
-  // ScrollView,
-  // StatusBar,
-  // StyleSheet,
-  Text,
-  // useColorScheme,
-  // View,
-} from 'react-native';
+export type RootStackParamList = {
+  Home: undefined;
+  Screen2: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView className="flex flex-row justify-center">
-      <Text className='text-customcolor'>Hello World</Text>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Screen2"
+          component={Screen2}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
